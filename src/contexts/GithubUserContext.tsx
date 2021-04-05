1cfbd0bf7a6f393855a1668bc9273acfbb9ea9eb
fallying <jonasexplore@gmail.com>
2021-04-05 17:29:32 -0300

    new components with styled-components.

--- src/contexts/GithubUserContext.tsx
+++ src/contexts/GithubUserContext.tsx
@@ -1,30 +1,54 @@
-import { createContext, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
-interface GithubData {
+interface LoginProviderProps {
+  children: ReactNode;
+  nameUser: string;
+  avatar_url: string;
+}
+
+export interface GithubData {
   avatar_url: string;
   name: string;
 }
 
-export const GithubUserContext = createContext({});
+interface contextData {
+  setLoggedUser: (username: string) => void;
+  getLoggedUser: () => GithubData;
+  getUserGithub: (username: string) => Promise<GithubData>;
+}
 
-export function GithubUserProvider({ children }) {
-  const [user, setUser] = useState({} as GithubData);
+export const GithubUserContext = createContext({} as contextData);
 
-  async function setLoggerUser(username: string) {
-    const githubUser = await (
+export function GithubUserProvider({ children, ...rest }: LoginProviderProps) {
+  const [user, setUser] = useState(
+    { avatar_url: rest.avatar_url, name: rest.nameUser } ?? ({} as GithubData)
+  );
+
+  async function getUserGithub(username: string): Promise<GithubData> {
+    const response = await (
       await fetch(`https://api.github.com/users/${username}`)
     ).json();
 
-    setUser({ avatar_url: githubUser.avatar_url, name: githubUser.name });
+    return { avatar_url: response.avatar_url, name: response.name };
   }
+
+  async function setLoggedUser(username: string) {
+    setUser(await getUserGithub(username));
+  }
+
+  function getLoggedUser(): GithubData {
+    return user;
+  }
+
+  useEffect(() => {
+    Cookies.set("avatar_url", String(user.avatar_url));
+    Cookies.set("name", String(user.name));
+  }, [user]);
 
   return (
     <GithubUserContext.Provider
-      value={{
-        user,
-        setUser,
-        setLoggerUser,
-      }}
+      value={{ getLoggedUser, setLoggedUser, getUserGithub }}
     >
       {children}
     </GithubUserContext.Provider>
